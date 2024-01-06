9066f79551f6d8b825e2c7b1d2312092594e085e
Haisenberg <1paf1@ukr.net>
2024-01-06 16:11:07 +0200

    first task

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 #hello
 
+'''try:
+    user_select = int(input("Введіть число від 1 до 7:"))
+    match userselect:
+        case 1:
+            print("Понеділок")
+        case 2:
+            print("Вівторок")
+        case 3:
+            print("Середа")
+        case 4:
+            print("Четверг")
+        case 5:
+            print("П'ятниця")
+        case 6:
+            print("Субота")
+        case 7:
+            print("Неділя")
+        case :
+            print("Введене число не входить в діапазон від 1 до 7.")
+except ValueError:
+    print("Введіть коректне число.")'''
